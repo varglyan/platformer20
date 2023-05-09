2d41894e9b3776fec8fcb1379c747b1ce96a5a99
na <na>
2023-05-09 17:13:52 +0200

    Add UFO to the scene

---
+++ Assets/Scripts/Mechanics/UfoController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Platformer.Mechanics
+{
+    public class UfoController : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _ufoRenderer;
+        [SerializeField] private AssetReference _ufoSpriteRef;
+
+        void Awake()
+        {
+            if (_ufoSpriteRef != null)
+                SetSprite();
+        }
+
+        private void OnDestroy()
+        {
+            _ufoSpriteRef?.ReleaseAsset();
+        }
+
+        private void SetSprite()
+        {
+            AsyncOperationHandle handle = _ufoSpriteRef.LoadAssetAsync<Sprite>();
+            handle.Completed += OnLoadCompleted;
+            void OnLoadCompleted(AsyncOperationHandle h)
+            {
+                if (h.Status != AsyncOperationStatus.Succeeded) return;
+                _ufoRenderer.sprite = (Sprite)h.Result;
+            }
+        }
+    }
+}
